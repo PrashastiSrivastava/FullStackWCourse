1302247ca0989d99781abaf1ea83251a1f900a99
PrashastiSrivastava <prashastisrivastava5@gmail.com>
2026-04-18 15:20:11 +0530

    first commit

---
+++ day 4/1-EcommerceOrderManagementSystem/1-EcommerceOrderManagementSystem/Welcome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_EcommerceOrderManagementSystem
+{
+    internal class Welcome
+    {
+        public void welcome() {
+            Console.WriteLine("Hello peeps");
+        }
+    }
+}
